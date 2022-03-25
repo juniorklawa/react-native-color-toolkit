0b42c7c422e2fa39bb5cd70eb977306532310dd4
everaldo@bb-8 <juniorklawa10@gmail.com>
2022-03-25 15:48:39 -0300

    feat: added first test

---
+++ src/combination/analogous.spec.ts
@@ -0,0 +1,9 @@
+import analogous from "./analogous";
+
+describe("analogous function", () => {
+  it("should return an array of analogous colors, given a color", () => {
+    const colors = analogous("#2196F3");
+
+    expect(colors).toEqual(["#2196F3", "#212df3", "#21f3e7"]);
+  });
+});
